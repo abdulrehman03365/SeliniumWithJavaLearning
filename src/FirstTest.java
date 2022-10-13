128e80744611332784d45d34e7b2f5245a72359f
abdulrehman03365 <89084033+aamir8cb@users.noreply.github.com>
2022-10-13 20:32:51 +0500

    Seond Commit

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -8,23 +9,29 @@ public class FirstTest {
     WebDriver driver;
     //Store current project workspace location in a string variable ‘path’
    
-    // @BeforeTest
-    // public void SetDriver(){
+    @BeforeTest
+    public void SetDriver(){
     // //Mention the location of ChromeDriver in localsystem
-    // System.setProperty("webdriver.chrome.driver","F:\\JavaSeliniumLearning\\SeliniumLearning\\src\\drivers\\chromedriver.exe");
-    // driver = new ChromeDriver();// Object is created- Chrome browser is opened
-    // driver.manage().window().maximize();
-    // }
-    // @Test
-    // public void verifyTitle()
-    // {
-    // driver.get("https://www.amazon.com");
-    // // String ActualTitle = driver.getTitle();
-    // // String ExpectedTitle = “Amazon.com: Online Shopping for Electronics, Apparel,
-    // //     Computers, Books, DVDs &amp; more”;
-    // // Assert.assertEquals(ActualTitle, ExpectedTitle);
-    // // System.out.println(“Assert passed”);
-    // }
+    System.setProperty("webdriver.chrome.driver","F:\\JavaSeliniumLearning\\SeliniumLearning\\src\\drivers\\chromedriver.exe");
+     driver = new ChromeDriver();// Object is created- Chrome browser is opened
+     driver.manage().window().maximize();
+    
+     }
+    
+    @Test
+    public void verifyTitle()
+    {
+  
+    driver.get("https://www.amazon.com");
+    driver.findElement(By.linkText("Registry")).click();
+    
+
+    // String ActualTitle = driver.getTitle();
+    // String ExpectedTitle = “Amazon.com: Online Shopping for Electronics, Apparel,
+    //     Computers, Books, DVDs &amp; more”;
+    // Assert.assertEquals(ActualTitle, ExpectedTitle);
+    // System.out.println(“Assert passed”);
+    }
     @AfterTest
     public void closedriver(){
     //closes the browser instance
@@ -32,13 +39,23 @@ public class FirstTest {
 
 
 
-    @Test
+    @Test(priority = 2)
     public void Test2(){
         int a=4;
+        boolean is_student ;
     Assert.assertEquals(4, a);
-    Assert.assertTrue(false);
+    is_student=true;
+    Assert.assertTrue(is_student);
         
     }
+
+
+
+@Test(priority = 1)
+public void Test3(){
+
+
+}
 
 }
 
