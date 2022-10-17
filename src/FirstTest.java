6790a7cf0b5f6f09b7f5fbe6db755f5bcc405e73
abdulrehman03365 <89084033+aamir8cb@users.noreply.github.com>
2022-10-17 20:13:14 +0500

    Add Test to switch between windows

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,20 @@
+import java.sql.Driver;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.security.auth.x500.X500Principal;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;;
 public class FirstTest {
     WebDriver driver;
     //Store current project workspace location in a string variable ‘path’
@@ -23,8 +33,12 @@ public class FirstTest {
     {
   
     driver.get("https://www.amazon.com");
-    driver.findElement(By.linkText("Registry")).click();
-    
+    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+    driver.findElement(By.xpath("//input[contains(@class,'a-button-input')]")).click();
+    wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Registry"))).click();
+    driver.navigate().back();
+
+    driver.navigate().forward();
 
     // String ActualTitle = driver.getTitle();
     // String ExpectedTitle = “Amazon.com: Online Shopping for Electronics, Apparel,
@@ -32,10 +46,41 @@ public class FirstTest {
     // Assert.assertEquals(ActualTitle, ExpectedTitle);
     // System.out.println(“Assert passed”);
     }
+
+
+    @Test
+    public void switchBetweenWindows () {
+        driver.get("https://the-internet.herokuapp.com/windows");
+       String Current_window_handle =driver.getWindowHandle();
+        
+        driver.findElement(By.xpath("//a[normalize-space()='Click Here']")).click();
+       Set<String> handlenames =driver.getWindowHandles();
+       Iterator<String> handlIterator = handlenames.iterator();
+       while(handlIterator.hasNext())
+       {
+        String child_window = handlIterator.next();
+
+        if(!Current_window_handle.equals(child_window))
+        {
+            driver.switchTo().window(child_window);
+            driver.findElement(By.xpath("//h3[normalize-space()='New Window']"));
+           
+            
+        }
+       
+    
+    }
+
+    // driver.switchTo().window(Current_window_handle);
+    // driver.close();
+    }
+
     @AfterTest
     public void closedriver(){
     //closes the browser instance
-    driver.close(); }
+    driver.close();
+driver.quit();
+}
 
 
 
